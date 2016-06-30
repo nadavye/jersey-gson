896640fc677683e5779a99db2d4a5506d13bc293
nadavye <nadav@sealights.io>
2016-06-30 18:24:15 +0300

    Update CucumberTest.java

--- src/test/java/com/ideoplex/tutorial/CucumberTest.java
+++ src/test/java/com/ideoplex/tutorial/CucumberTest.java
@@ -5,7 +5,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin = {"json:target/cucumber-report.json",
-                           "html:target/cucumber-report"})
+                           "html:target/cucumber-report"},
+                           glue = "uk.co.automatictester.jwebfwk.glue")
 public class CucumberTest extends AbstractTestNGCucumberTests
 {
 }
